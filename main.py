4366eb4634c65f0234e2ee7d3d8ebe13ba6dee72
vasarm <vasarmartin0@gmail.com>
2021-05-23 23:56:44 +0300

    main.py has now config file.

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import sys
 import os
+import json
 
 from simulation.simulation import Simulation
 from simulation.array_from_picture import convert_img_to_array
 
 
-try:
-    # If picture file entered then use it
-    lattice = None
-    filename = sys.argv[1]
-    file_path = os.path.dirname(__file__)
-    lattice = convert_img_to_array(file_path, filename)
-    random = False
-except IndexError:
-    random = True
-except FileNotFoundError:
-    print("Could not find entered file in this folder. Check file name and it's location.")
-    sys.exit(0)
+with open("config.json") as f:
+    settings = json.load(f)
 
-sim = Simulation(K=0.5, T=0.1, I=0.001, width=1000, height=1000)
-sim.init(random=random, lattice=lattice)
-sim.run(number_of_steps=0, count_lattice_step=10, save=True)
-sim.display_result(y_axis_type="%")
+try:
+    if settings["lattice_path"] != "-1":
+        if os.path.isfile(settings["lattice_path"]):
+            file_name = os.path.basename(settings["lattice_path"])
+            file_path = os.path.dirname(settings["lattice_path"])
+            lattice = convert_img_to_array(file_path, file_name)
+            settings["random"] = False
+        else:
+            print("Enetered lattice path is not file.")
+            sys.exit(-1)
+except:
+    print("Problem with settings 'lattice_path' variable. Set '-1' if no input.")
+    sys.exit(-1)
+
+
+sim = Simulation(K=settings["K"], T=settings["T"], I=settings["I"],
+                 width=settings["width"], height=settings["height"])
+sim.init(random=settings["random"], lattice=lattice)
+sim.run(number_of_steps=settings["number_of_steps"],
+        count_lattice_step=settings["count_lattice_step"], save=settings["save"])
+if settings["display_result"]:
+    sim.display_result(y_axis_type="%")
